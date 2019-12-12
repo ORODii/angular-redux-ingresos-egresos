cff98c87ede70655b1e062f0373e9aa03df24884
Andres Orozco <aorozco@simetrical.com.mx>
2019-12-11 19:01:30 -0600

    - Se guarda la subscription en la variable de la clase

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent implements OnInit, OnDestroy {
   constructor(private authService: AuthService, private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.store.select('ui')
+    this.subscription = this.store.select('ui')
       .subscribe(ui => {
         this.cargando = ui.isLoading;
       });
